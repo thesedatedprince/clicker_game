61936df01f81b2c1e5ea1bd9808dd3ad45f661ad
Matthew Perry <thesedatedprince@gmail.com>
2017-05-10 13:37:36 +0100

    added button blocks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,6 +8,10 @@ class Counter extends Component{
     }
   }
 
+  tooLow(level, check){
+    return level > check
+  }
+
     render(){
       const {value, increment, second_level, third_level} = this.props;
 
@@ -15,13 +19,15 @@ class Counter extends Component{
       <p>
             Score: {value}
           <button onClick={increment}>
-            Click
+            Punch
           </button>
           <button onClick={this.unlock(value, 80, second_level)}>
-            Mega Click
+            Mega Punch
+            {this.tooLow(value, 80) ? <div className="label">Level too Low!</div> : null}
           </button>
           <button onClick={this.unlock(value, 50, third_level)}>
-            Giga Click
+            Giga Punch
+            {this.tooLow(value, 50) ? <div className="label">Level too Low!</div> : null}
           </button>
       </p>
     )
